44a16e53dc1322b4e96e81d665cfc6702c2b36d1
Pauline <gilgpauline@gmail.com>
2020-06-15 20:12:22 +0200

    minor updates

--- src/components/darkmode.js
+++ src/components/darkmode.js
@@ -6,9 +6,6 @@ function darkmodebutton() {
         var button = document.getElementById('dark-mode-button'); 
         var switchButton = document.getElementById('dark-mode-switch'); 
         var body = document.body;
-        var sun = document.getElementById('dark-mode-button-sun');
-        var moon = document.getElementById('dark-mode-button-moon');
-        var projects = document.getElementsByClassName('project');
 
 
         if (switchButton.getAttribute('data-value') == 'light') {
@@ -18,8 +15,6 @@ function darkmodebutton() {
             body.style.backgroundColor = '#212121';
             body.style.color = '#fff';
             switchButton.setAttribute('data-value', 'dark');
-            sun.style.display = 'none';
-            moon.style.display = 'block';
         }
         else {
             switchButton.style.left = '2px';
@@ -28,8 +23,6 @@ function darkmodebutton() {
             body.style.backgroundColor = '#F5F5F5';
             body.style.color = '#212121';
             switchButton.setAttribute('data-value', 'light');
-            sun.style.display = 'block';
-            moon.style.display = 'none';
         } 
     }
 
@@ -37,8 +30,6 @@ function darkmodebutton() {
 
         <div id="dark-mode-button" onClick={darkModeSwitch}>
             <div id="dark-mode-switch" data-value="light"></div>
-            <span id="dark-mode-button-sun">☀️</span>
-            <span id="dark-mode-button-moon">🌙</span>
         </div>
     );
 }
--- src/components/nav.js
+++ src/components/nav.js
@@ -5,7 +5,7 @@ function nav() {
   return (
     <nav>
       <a id="linkedin" href="https://www.linkedin.com/in/pauline-gilg/" target="_blank">LinkedIn <span>💼</span></a>
-      <a id="blog" href="https://paulinegilg.eu/blog" target="_blank">Blog <span>✍️</span></a>
+      <a id="blog" href="/blog" target="_blank">Blog <span>✍️</span></a>
       <a id="github" href="https://github.com/pompompolin" target="_blank">Github <span>💡</span></a>
       <a id="codepen" href="https://codepen.com/pompompolin" target="_blank">Codepen <span>🎨</span></a>
       <a id="twitter" href="https://twitter.com/pompompolin" target="_blank">Twitter <span>💬</span></a>
